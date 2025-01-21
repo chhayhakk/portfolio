f6b0dad2729dbdd5eaa5e9d22bcf8e6ac3a74a4e
chhayhakk <chhayhakkourk@gmail.com>
2025-01-21 16:02:58 +0700

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: CONFIG.base || '/portfolio',
+  // base: CONFIG.base || '/portfolio',
+  base: '/portfolio/',
   plugins: [
     react(),
     createHtmlPlugin({
